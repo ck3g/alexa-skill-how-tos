250a14874cca2255aed45cf7dcea441efb8d2e44
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2018-09-02 12:04:26 +0200

    Toy robot simulator initial commit

---
+++ how-to-use-session-attributes/README.md
@@ -0,0 +1,3 @@
+# Howto use Session Attributes
+
+The demo of the Alexa Skill implementation for ["How to use Session Attributes in Alexa skills"](http://whatdidilearn.info/) blog post.
---
+++ how-to-use-session-attributes/lambda/custom/index.js
@@ -0,0 +1,104 @@
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+
+const Alexa = require('ask-sdk-core');
+
+const CARD_TITLE = 'The Toy Robot Simulator';
+const HELP_MESSAGE = 'You can place your robot to a default position using "Place" command. Once the robot is in the position you can move it using "move" command, rotate it using "turn left" or "turn right" commands. In between, you can use the "Report" command to ask for its current position.';
+const WELCOME_MESSAGE = `Welcome to the toy robot simulator! ${HELP_MESSAGE}`;
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+      .speak(WELCOME_MESSAGE)
+      .reprompt(WELCOME_MESSAGE)
+      .withSimpleCard(CARD_TITLE, WELCOME_MESSAGE)
+      .getResponse();
+  },
+};
+
+const PlaceIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'PlaceIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'The robot has been places into its initial position.'
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard(CARD_TITLE, speechText)
+      .getResponse();
+  },
+};
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+      .speak(HELP_MESSAGE)
+      .reprompt(HELP_MESSAGE)
+      .withSimpleCard(CARD_TITLE, HELP_MESSAGE)
+      .getResponse();
+  },
+};
+
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    const speechText = 'Session termindated!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard(CARD_TITLE, speechText)
+      .getResponse();
+  },
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('Sorry, I can\'t understand the command. Please say again.')
+      .reprompt('Sorry, I can\'t understand the command. Please say again.')
+      .getResponse();
+  },
+};
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    PlaceIntentHandler,
+    HelpIntentHandler,
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
